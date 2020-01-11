00b3fbbfa06ba2482666958139f8ae2d4f074fd8
anand <anands9288907423@gmail.com>
2020-01-11 11:15:22 +0530

    test

--- index.js
+++ index.js
@@ -2,29 +2,36 @@ var http = require('http');
 const mbxClient = require('@mapbox/mapbox-sdk');
 var polyline = require('@mapbox/polyline');
 const mbxDirection = require('@mapbox/mapbox-sdk/services/directions');
-
+var url = require('url');
 
     // creating mapbox client
     const baseClient = mbxClient({ accessToken: "pk.eyJ1IjoiYW5hbmQ5Mjg4IiwiYSI6ImNrNHk2dHJpdDA3dHEzZm82Y2hnY252cjEifQ.W-3fm0taJg_noVA_zzJO7g" });
     const direction_service = mbxDirection(baseClient);
-
+    var jsonObj = [];
+    // creating a for loop 
+ 
 exports.Mycloud = http.createServer(function (req, res) {
+  var q = url.parse(req.url, true).query;
+  var txt = q.waypoints;
+  console.log(txt);
+  var waypoints_array = txt.split(';');
+  console.log(waypoints_array[0],"way point test")
+  for(i=0;i<waypoints_array.length;i++){
+    value = waypoints_array[i];
+    console.log(value,"for each");
+    cords = value.split(",");
+    item = {}
+    item["coordinates"] = [Number(cords[0]),Number(cords[1])];
+    console.log(item,"obj created");
+    jsonObj.push(item);
+  }
+    
+    console.log(jsonObj,"the js array")
+
     direction_service.getDirections({
-        profile: 'driving-traffic',
-        waypoints: [
-          {
-            coordinates: [13.4301, 52.5109],
-            approach: 'unrestricted'
-          },
-          {
-            coordinates: [13.4265, 52.508]
-          },
-          {
-            coordinates: [13.4194, 52.5072],
-            bearing: [100, 60]
-          }
-        ]
-      })
+      profile: 'driving-traffic',
+      waypoints: jsonObj
+    })
         .send()
         .then(responses => {
           // response.send("hai");
