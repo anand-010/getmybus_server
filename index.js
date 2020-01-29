142711585a173f0893b204c67d5542d1c43aa46c
anand <anands9288907423@gmail.com>
2020-01-29 18:50:02 +0530

    last change

--- index.js
+++ index.js
@@ -1,63 +1,62 @@
 var http = require('http');
-const mbxClient = require('@mapbox/mapbox-sdk');
-var polyline = require('@mapbox/polyline');
-const mbxDirection = require('@mapbox/mapbox-sdk/services/directions');
+var admin = require("firebase-admin");
+var serviceAccount = require("./key.json");
 var url = require('url');
+var turf = require('@turf/turf');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://getmybus-e8c7d.firebaseio.com"
+});
+var db = admin.firestore();
+var dbsnapshot;
+// receiving the database at the start 
+db.collection("users").onSnapshot((snapshot) => {
+    dbsnapshot = snapshot;
+    console.log("db received");
+  }, (error) => {
+    //...
+    console.log("some error occured "+error);
+  });
 
-    // creating mapbox client
-    const baseClient = mbxClient({ accessToken: "pk.eyJ1IjoiYW5hbmQ5Mjg4IiwiYSI6ImNrNHk2dHJpdDA3dHEzZm82Y2hnY252cjEifQ.W-3fm0taJg_noVA_zzJO7g" });
-    const direction_service = mbxDirection(baseClient);
-    var jsonObj = [];
-    var waypoints_array="2.6;1.5";
-    // creating a for loop 
- 
 exports.Mycloud = http.createServer(function (req, res) {
-  var q = url.parse(req.url, true).query;
-  var txt = q.waypoints;
-  if(txt!=undefined){
-    console.log(txt);
-  console.log("my ft call");
-    waypoints_array = txt.split(';');
-  console.log(waypoints_array[0],"way point test")
-  for(i=0;i<waypoints_array.length;i++){
-    value = waypoints_array[i];
-    console.log(value,"for each");
-    cords = value.split(",");
-    item = {}
-    item["coordinates"] = [Number(cords[0]),Number(cords[1])];
-    console.log(item,"obj created");
-    jsonObj.push(item);
-  }
-    
-    console.log(jsonObj,"the js array")
+  var q =  url.parse(req.url, true).query;
+  var check_array = new Array();
 
-    direction_service.getDirections({
-      profile: 'driving-traffic',
-      waypoints: jsonObj
-    })
-        .send()
-        .then(responses => {
-          // response.send("hai");
-          const directions = responses.body;
-          var ans = polyline.toGeoJSON(directions.routes[0].geometry);
-          console.log(ans);
-          res.write(JSON.stringify(ans));
-          res.end();
-          jsonObj.length = 0;
-          // // need to be send to the Android application for display routes in google maps android phone
-          // console.log(polyline.toGeoJSON(directions.routes[0].geometry).coordinates);
-          // response.send(polyline.toGeoJSON(directions.routes[0].geometry))
-        })
-        .catch(() => {
-          res.write("some bad happend");
-          console.error('Do that');
-          res.end();
-      })
-  // res.write('Hello World!');
-  // res.end();
-  }
-  else{
-    res.write("plese keep not empty");
-    res.end();
-  }
+      var src = q.src;
+      var dest = q.dest;
+      if(src !=null && dest != null){
+        src = src.split(",").map(Number);
+      dest = dest.split(",").map(Number);
+      console.log(dbsnapshot , src , dest);
+      var i =0;
+      dbsnapshot.forEach(function(snap){
+        var route_array = new Array();
+        var stops = snap.data().stops;
+        var routes = snap.data().route;
+        routes.forEach(points => {
+          route_array.push([points.latitude,points.longitude]);
+          
+        });
+        // converting the arrays into turf array
+        var line = turf.lineString(route_array);
+        // new point in our project it is the wifi points from the bus location
+        var source = turf.point(src);
+        var destination = turf.point(dest);
+        // finding the nearest point on the line to the line
+        var snapped = turf.nearestPointOnLine(line, source, {units: 'meters'});
+        var snapped_ = turf.nearestPointOnLine(line, destination, {units: 'meters'});
+        res.write("snapped");
+        res.write("snapped_");
+        console.log(snapped_.properties.dist, snapped.properties.dist,i++);
+        if(snapped.properties.dist <=20.00){
+          if(snapped_.properties.dist <=20.00){
+            check_array.push(snap.id.toString());
+          }
+        }
+      });
+      }
+      console.log(check_array.toString());
+      res.write("everything is fine");
+      res.end();
+
 }).listen(process.env.PORT || 5000);
