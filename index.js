9f1c18765774e16b7252bd2af448790a2e2fc3dd
anand <anands9288907423@gmail.com>
2020-02-02 14:45:29 +0530

    wow

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./key.json");
 var url = require('url');
 var turf = require('@turf/turf');
+const lineSlice = require('@turf/line-slice');
+const lineDistance = require('@turf/line-distance');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://getmybus-e8c7d.firebaseio.com"
@@ -10,7 +13,7 @@ admin.initializeApp({
 var db = admin.firestore();
 var dbsnapshot;
 // receiving the database at the start 
-db.collection("users").onSnapshot((snapshot) => {
+db.collection("rides").onSnapshot((snapshot) => {
     dbsnapshot = snapshot;
     console.log("db received");
   }, (error) => {
@@ -18,7 +21,7 @@ db.collection("users").onSnapshot((snapshot) => {
     console.log("some error occured "+error);
   });
 
-exports.Mycloud = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   var q =  url.parse(req.url, true).query;
   var check_array = new Array();
 
@@ -33,6 +36,9 @@ exports.Mycloud = http.createServer(function (req, res) {
         var route_array = new Array();
         var stops = snap.data().stops;
         var routes = snap.data().route;
+        var start_point = snap.data().starting_point;
+        console.log("starting point is", start_point);
+        
         routes.forEach(points => {
           route_array.push([points.latitude,points.longitude]);
           
@@ -42,16 +48,34 @@ exports.Mycloud = http.createServer(function (req, res) {
         var line = turf.lineString(route_array);
 
         // new point in our project it is the wifi points from the bus location
+        var start_point_p = turf.point([start_point.latitude, start_point.longitude])
         var source = turf.point(src);
         var destination = turf.point(dest);
 
-        // finding the nearest point on the line to the line
         var snapped = turf.nearestPointOnLine(line, source, {units: 'meters'});
         var snapped_ = turf.nearestPointOnLine(line, destination, {units: 'meters'});
         console.log(snapped_.properties.dist, snapped.properties.dist,i++);
-        if(snapped.properties.dist <=20.00){
-          if(snapped_.properties.dist <=20.00){
-            check_array.push(snap.id.toString());
+        if(snapped.properties.dist <=100.00){
+          if(snapped_.properties.dist <=100.00){
+
+                  // todo get the currect starting point of the bus
+                  // not the routest starting point this is a test
+            var sliced_src = lineSlice(start_point_p, snapped, line);
+            var sliced_dest = lineSlice(start_point_p, snapped_, line);
+            var length_src = lineDistance(sliced_src, 'meters');
+            var length_dest = lineDistance(sliced_dest, 'meters');
+            console.log(length_src, length_dest,"this is the length");
+            if(length_src<length_dest){
+              // select these bus because these are running in same location as customer expect
+              // assume that the current posion of the bus is in the 15th coordinate in polyline route then
+              var sliced_current = lineSlice(start_point_p, line.geometry.coordinates[15], line);
+              var length_current = lineDistance(sliced_current, 'meters');
+              console.log(length_current,"value");
+              
+              if (length_dest > length_current) {
+                check_array.push(snap.id.toString());
+              }
+            }
           }
         }
       });
--- test.js
+++ test.js
@@ -1,11 +1,30 @@
 var http = require('http');
 var url = require('url');
+var admin = require("firebase-admin");
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://getmybus-e8c7d.firebaseio.com"
+});
+var db = admin.firestore();
+var dbsnapshot;
+// receiving the database at the start 
+db.collection("test").onSnapshot((snapshot) => {
+    dbsnapshot = snapshot;
+    console.log("db received");
+  }, (error) => {
+    //...
+    console.log("some error occured "+error);
+  });
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
 //   var txt = q.name[0] + " " + q.name[1];
-var txt = q.watpoints;
-var nameArr = txt.split(';');
-  res.end(nameArr[0]);
+dbsnapshot.forEach(function(snap){
+  console.log(snap.data());
+  
+    
+  });
+  res.end();
 }).listen(8080);
